3b89471939635076b614255b0b303411cae36887
Michael W Chung <dryhslaw@gmail.com>
2018-10-15 00:22:05 -0400

    acc

--- master.js
+++ master.js
@@ -98,18 +98,18 @@ function start() {
 }
 
 // Fade in function (test)
-function unfade(element) {
-    var opac = 0.1;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (opac >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = opac;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
-}
+// function unfade(element) {
+//     var opac = 0.1;  // initial opacity
+//     element.style.display = 'block';
+//     var timer = setInterval(function () {
+//         if (opac >= 1){
+//             clearInterval(timer);
+//         }
+//         element.style.opacity = opac;
+//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op += op * 0.1;
+//     }, 10);
+// }
 
 // Runs start function upon load.
-// window.addEventListener('load', start);
+window.addEventListener('load', start);
